fb1d4061f36eebca3d045d68cca327ce2828f838
jithmiw <wjithmiravihari@gmail.com>
2022-10-13 21:56:57 +0530

    Updated the content of index controller

---
+++ assignments/js/spa/controller/IndexController.js
@@ -0,0 +1,39 @@
+// NAVIGATIONS
+$(document).ready(function(){
+    $('#home').css('display', 'block');
+    $('#orders, #customers, #items, #order-details').css('display', 'none');
+    setDashboard();
+});
+
+$('#home-link').on('click', function () {
+    $('#home').css('display', 'block');
+    $('#orders, #customers, #items, #order-details').css('display', 'none');
+    setDashboard();
+});
+
+$('#orders-link').on('click', function () {
+    $('#orders').css('display', 'block');
+    $('#home, #customers, #items, #order-details').css('display', 'none');
+});
+
+$('#customers-link').on('click', function () {
+    $('#customers').css('display', 'block');
+    $('#home, #orders, #items, #order-details').css('display', 'none');
+});
+
+$('#items-link').on('click', function () {
+    $('#items').css('display', 'block');
+    $('#home, #orders, #customers, #order-details').css('display', 'none');
+});
+
+$('#orderDetails-link').on('click', function () {
+    $('#order-details').css('display', 'block');
+    $('#home, #orders, #customers, #items').css('display', 'none');
+});
+
+// SETTING DASHBOARD
+function setDashboard() {
+    $('#noOfCustomers').text(customers.length);
+    $('#noOfItems').text(items.length);
+    $('#noOfOrders').text(orders.length);
+}
