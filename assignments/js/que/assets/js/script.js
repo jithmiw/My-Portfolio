9ef93027b1002f353e6159edc235ef45ffb21831
jithmiw <wjithmiravihari@gmail.com>
2022-11-10 21:30:58 +0530

    Content updated

--- assignments/js/que/assets/js/script.js
+++ assignments/js/que/assets/js/script.js
@@ -1,13 +1,14 @@
-var cueData={
-    persons:['A', 'B', 'C', 'D', 'E', 'F'],
-    nextPerson:function (){
-        var lastPerson= this.persons.pop();
+var cueData = {
+    persons: ['A', 'B', 'C', 'D', 'E', 'F'],
+    nextPerson: function () {
+        var lastPerson = this.persons.pop();
         this.persons.unshift(lastPerson);
     }
 }
 
 renderQue();
-function renderQue(){
+
+function renderQue() {
     $('#container').empty();
     for (let i = 0; i < cueData.persons.length; i++) {
         $('#container').append(`<div><h2>${cueData.persons[i]}</h2></div>`);
@@ -15,4 +16,4 @@ function renderQue(){
     cueData.nextPerson();
 }
 
-// setInterval(renderQue,1000);
+setInterval(renderQue, 1000);
